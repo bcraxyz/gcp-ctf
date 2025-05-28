211d2c04995c9c65f39638b820d87b49c2243637
bcraxyz <114228200+bcraxyz@users.noreply.github.com>
2025-05-29 00:02:47 +0800

    Update project.tf

--- project.tf
+++ project.tf
@@ -4,15 +4,15 @@ resource "google_folder" "ctf_folder" {
 }
 
 resource "google_project" "project_a" {
-  name            = "Challenge 01 Project A"
-  project_id      = "c01-proj-a"
+  name            = "Challenge 01 - Project A"
+  project_id      = "ctf-c01-proj-a"
   folder_id       = google_folder.ctf_folder.name
   billing_account = var.billing_account_id
 }
 
 resource "google_project" "project_b" {
-  name            = "Challenge 01 Project B"
-  project_id      = "c01-proj-b"
+  name            = "Challenge 01 - Project B"
+  project_id      = "ctf-c01-proj-b"
   folder_id       = google_folder.ctf_folder.name
   billing_account = var.billing_account_id
 }

d4b7ee0c9eae4eca97dd1bbd5bb4098c7917b84e
bcraxyz <114228200+bcraxyz@users.noreply.github.com>
2025-05-28 19:33:12 +0800

    Create iam.tf

---
+++ iam.tf
@@ -0,0 +1,31 @@
+resource "google_service_account" "vm_sa" {
+  provider     = google.project_a
+  account_id   = "c01-vm-sa"
+  display_name = "VM Service Account"
+  project      = var.project_a
+}
+
+resource "google_service_account" "gcs_sa" {
+  provider     = google.project_b
+  account_id   = "c01-gcs-sa"
+  display_name = "GCS Service Account"
+  project      = var.project_b
+}
+
+resource "google_project_iam_member" "vm_sa_impersonate_gcs_sa" {
+  project = var.project_b
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.vm_sa.email}"
+}
+
+resource "google_storage_bucket_iam_member" "gcs_sa_bucket_admin" {
+  bucket = google_storage_bucket.sensitive_data.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.gcs_sa.email}"
+}
+
+resource "google_project_iam_member" "iap_access_group" {
+  project = var.project_a
+  role    = "roles/iap.tunnelResourceAccessor"
+  member  = "group:${var.ctf_users_group}"
+}

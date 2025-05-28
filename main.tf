78211383f1fde6ef89505825708ecc74fbf97a01
bcraxyz <114228200+bcraxyz@users.noreply.github.com>
2025-05-28 19:54:51 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,26 @@
 provider "google" {
-  project = var.project_a
+  alias   = "project_a" 
+  project = google_project.project_a.project_id
   region  = var.region
   zone    = var.zone
 }
 
 provider "google" {
   alias   = "project_b"
-  project = var.project_b
+  project = google_project.project_b.project_id
   region  = var.region
   zone    = var.zone
 }
 
 resource "google_project_service" "project_a_apis" {
+  provider = google.project_a
   for_each = toset([
     "compute.googleapis.com",
     "iam.googleapis.com",
     "storage.googleapis.com",
     "iap.googleapis.com"
   ])
-  project = var.project_a
+  project = google_project.project_a.project_id
   service = each.key
 }
 
@@ -28,6 +30,6 @@ resource "google_project_service" "project_b_apis" {
     "iam.googleapis.com",
     "storage.googleapis.com"
   ])
-  project = var.project_b
+  project = google_project.project_b.project_id
   service = each.key
 }

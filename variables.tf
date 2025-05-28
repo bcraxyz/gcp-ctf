62eded09d2bac25dc664c354971892c9cd12c46b
bcraxyz <114228200+bcraxyz@users.noreply.github.com>
2025-05-28 19:32:39 +0800

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "project_a" { 
+  default = "c01-proj-a" 
+}
+
+variable "project_b" { 
+  default = "c01-proj-b" 
+}
+
+variable "region" { 
+  default = "asia-southeast1" 
+}
+
+variable "zone" { 
+default = "asia-southeast1-a" 
+}
+
+variable "ctf_users_group" {
+  default = "ctf-users@yourdomain.com"
+}

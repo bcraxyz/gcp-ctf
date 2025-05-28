17f21c052125511412d77b94ebd8959de518fa93
bcraxyz <114228200+bcraxyz@users.noreply.github.com>
2025-05-28 19:50:47 +0800

    Update storage.tf

--- storage.tf
+++ storage.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "sensitive_data" {
   name          = "c01-sensitive-data"
   location      = var.region
-  project       = var.project_b
+  project       = google_project.project_b.project_id
   force_destroy = true
 }
 

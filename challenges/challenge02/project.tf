c19c4e4a2c3d8f69046add34a93e86307ab13b36
bcraxyz <114228200+bcraxyz@users.noreply.github.com>
2025-06-23 23:44:54 +0800

    Create project.tf

---
+++ challenges/challenge02/project.tf
@@ -0,0 +1,20 @@
+resource "google_project" "project" {
+  name            = "Challenge 02 - Project"
+  project_id      = "ctf-c02-proj"
+  folder_id       = var.ctf_folder_id
+  billing_account = var.billing_account_id
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "google_project_service" "project_apis" {
+  provider = google.project
+  for_each = toset([
+    "resourcemanager.googleapis.com",
+    "iam.googleapis.com",
+  ])
+  project = google_project.project.project_id
+  service = each.key
+}

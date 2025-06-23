654087c3f2f86b32b59c4b1d48a5e89d644a6048
bcraxyz <114228200+bcraxyz@users.noreply.github.com>
2025-06-23 23:56:18 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,14 +11,16 @@ FOLDER_ID=$(terraform output -raw ctf_folder_id)
 cd ..
 
 # Deploy challenge(s)
-for CHALLENGE in challenges/challenge01; do
-  echo "[+] Deploying $CHALLENGE..."
-  cd $CHALLENGE || exit 1
+for CHALLENGE in challenges/*; do
+  if [ -d "$CHALLENGE" ]; then
+    echo "[+] Deploying $CHALLENGE..."
+    cd "$CHALLENGE" || exit 1
 
-  terraform init
-  terraform apply -auto-approve \
-    -var-file=../../terraform.tfvars \
-    -var="ctf_folder_id=$FOLDER_ID"
+    terraform init
+    terraform apply -auto-approve \
+      -var-file=../../terraform.tfvars \
+      -var="ctf_folder_id=$FOLDER_ID"
 
-  cd - > /dev/null
+    cd - > /dev/null
+  fi
 done

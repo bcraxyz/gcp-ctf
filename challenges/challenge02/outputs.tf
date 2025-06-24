d8a20396715af319c7467ef79fda66f589797a7e
bcraxyz <114228200+bcraxyz@users.noreply.github.com>
2025-06-24 19:08:05 +0800

    Update outputs.tf

--- challenges/challenge02/outputs.tf
+++ challenges/challenge02/outputs.tf
@@ -2,10 +2,6 @@ output "project_id" {
   value = google_project.project.project_id
 }
 
-output "gcs_service_account" {
-  value = google_service_account.gcs_sa.email
-}
-
 output "bucket_name" {
   value = google_storage_bucket.sensitive_data.name
 }

c149d71bb0c403b44c71e7080ae477731740d610
bcraxyz <114228200+bcraxyz@users.noreply.github.com>
2025-06-24 10:59:04 +0800

    Update main.tf

--- challenges/challenge01/main.tf
+++ challenges/challenge01/main.tf
@@ -1,10 +1,57 @@
-module "challenge01" {
-  source = "./"
+resource "google_project" "project_a" {
+  name            = "Challenge 01 - Project A"
+  project_id      = "ctf-c01-proj-a"
+  folder_id       = var.ctf_folder_id
+  billing_account = var.billing_account_id
 
-  region              = var.region
-  zone                = var.zone
-  org_id              = var.org_id
-  billing_account_id  = var.billing_account_id
-  ctf_users_group     = var.ctf_users_group
-  ctf_folder_id       = var.ctf_folder_id
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "google_project" "project_b" {
+  name            = "Challenge 01 - Project B"
+  project_id      = "ctf-c01-proj-b"
+  folder_id       = var.ctf_folder_id
+  billing_account = var.billing_account_id
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "google_project_service" "project_a_apis" {
+  provider = google.project_a
+  for_each = toset([
+    "compute.googleapis.com",
+    "iam.googleapis.com",
+    "storage.googleapis.com",
+    "iap.googleapis.com"
+  ])
+  project = google_project.project_a.project_id
+  service = each.key
+}
+
+resource "google_project_service" "project_b_apis" {
+  provider = google.project_b
+  for_each = toset([
+    "iam.googleapis.com",
+    "storage.googleapis.com"
+  ])
+  project = google_project.project_b.project_id
+  service = each.key
+}
+
+provider "google" {
+  alias   = "project_a"
+  project = google_project.project_a.project_id
+  region  = var.region
+  zone    = var.zone
+}
+
+provider "google" {
+  alias   = "project_b"
+  project = google_project.project_b.project_id
+  region  = var.region
+  zone    = var.zone
 }

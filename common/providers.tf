42851ba829138c4e5c971df27d970c6e98a53135
bcraxyz <114228200+bcraxyz@users.noreply.github.com>
2025-06-24 18:21:20 +0800

    Update providers.tf

--- common/providers.tf
+++ common/providers.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.40"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.7.2"
+    }
+  }
+}
+
 provider "google" {
   region = var.region
   zone   = var.zone

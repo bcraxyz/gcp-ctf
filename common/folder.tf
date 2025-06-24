efcc64374da805345281430705cd08092b75a20d
bcraxyz <114228200+bcraxyz@users.noreply.github.com>
2025-06-24 15:18:53 +0800

    Update folder.tf

--- common/folder.tf
+++ common/folder.tf
@@ -2,7 +2,3 @@ resource "google_folder" "ctf_folder" {
   display_name = "CTF"
   parent       = "organizations/${var.org_id}"
 }
-
-output "ctf_folder_id" {
-  value = google_folder.ctf_folder.id
-}

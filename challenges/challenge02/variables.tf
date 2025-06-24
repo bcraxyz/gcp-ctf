980c6b0d7cb1235ec7952fda2ee3cd7e29104150
bcraxyz <114228200+bcraxyz@users.noreply.github.com>
2025-06-24 10:50:35 +0800

    Update variables.tf

--- challenges/challenge02/variables.tf
+++ challenges/challenge02/variables.tf
@@ -1,11 +1,3 @@
-variable "ctf_folder_id" {
-  type = string
-}
-
-variable "billing_account_id" {
-  type = string
-}
-
-variable "region" {
-  default = "asia-southeast1"
-}
+variable "region" {}
+variable "billing_account_id" {}
+variable "ctf_folder_id" {}

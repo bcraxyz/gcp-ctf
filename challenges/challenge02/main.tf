85f2b3fd984581b9c4b16d715486cc8928523afd
bcraxyz <114228200+bcraxyz@users.noreply.github.com>
2025-06-24 11:02:54 +0800

    Update main.tf

--- challenges/challenge02/main.tf
+++ challenges/challenge02/main.tf
@@ -1,7 +1,24 @@
-module "challenge02" {
-  source = "./"
+resource "google_project" "project" {
+  name            = "Challenge 02 - Project"
+  project_id      = "ctf-c02-proj"
+  folder_id       = var.ctf_folder_id
+  billing_account = var.billing_account_id
 
-  region             = var.region
-  billing_account_id = var.billing_account_id
-  ctf_folder_id      = var.ctf_folder_id
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "google_project_service" "project_apis" {
+  for_each = toset([
+    "resourcemanager.googleapis.com",
+    "iam.googleapis.com",
+  ])
+  project = google_project.project.project_id
+  service = each.key
+}
+
+provider "google" {
+  project = google_project.project.project_id
+  region  = var.region
 }

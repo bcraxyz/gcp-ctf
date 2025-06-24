9cc4a372c354d72a6fa4f6c00e0afe88c08eab8e
bcraxyz <114228200+bcraxyz@users.noreply.github.com>
2025-06-24 18:24:04 +0800

    Update main.tf

--- challenges/challenge02/main.tf
+++ challenges/challenge02/main.tf
@@ -1,6 +1,10 @@
+resource "random_id" "suffix" {
+  byte_length = 2 # This will generate a 4-character hex string (2 bytes * 2 hex chars/byte)
+}
+
 resource "google_project" "project" {
   name            = "Challenge 02 - Project"
-  project_id      = "ctf-c02-proj"
+  project_id      = "ctf-c02-proj-${random_id.suffix.hex}"
   folder_id       = var.ctf_folder_id
   billing_account = var.billing_account_id
 

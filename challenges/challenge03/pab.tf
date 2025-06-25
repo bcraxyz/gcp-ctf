31045374f4acdd1f97cd5b6c74190b94d4df28ed
bcraxyz <114228200+bcraxyz@users.noreply.github.com>
2025-06-25 18:08:19 +0800

    Update pab.tf

--- challenges/challenge03/pab.tf
+++ challenges/challenge03/pab.tf
@@ -27,5 +27,5 @@ resource "google_access_context_manager_service_perimeter_resource" "attach_proj
 resource "google_access_context_manager_access_policy_iam_member" "bind_ctf_users" {
   name    = google_access_context_manager_access_policy.ctf_policy.name
   role    = "roles/accesscontextmanager.policyReader"
-  members = "group:${var.ctf_users_group}"
+  member  = "group:${var.ctf_users_group}"
 }
